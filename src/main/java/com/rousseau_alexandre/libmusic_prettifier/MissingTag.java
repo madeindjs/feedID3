98adfdc48e3674ccd46fd1abbc7968069b461faa
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-03-20 13:59:21 +0100

    Add 'getType' method for search param

--- src/main/java/com/rousseau_alexandre/libmusic_prettifier/MissingTag.java
+++ src/main/java/com/rousseau_alexandre/libmusic_prettifier/MissingTag.java
@@ -6,6 +6,16 @@ public enum MissingTag {
     Artist,
     Year,
     GenreDescription,
-    Id3
+    Id3;
+
+    /**
+     * The type for the GET search query
+     *
+     * @see
+     * https://www.discogs.com/developers/#page:database,header:database-search
+     */
+    public String getType() {
+        return this.getClass().getName().toLowerCase();
+    }
 
 }
