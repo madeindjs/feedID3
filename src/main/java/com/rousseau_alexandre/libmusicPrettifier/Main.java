b3add36097abec817fc8897a12be290e2e59e888
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-03-27 16:16:14 +0200

    Add recursive file search for librairy

--- src/main/java/com/rousseau_alexandre/libmusicPrettifier/Main.java
+++ src/main/java/com/rousseau_alexandre/libmusicPrettifier/Main.java
@@ -2,24 +2,13 @@ package com.rousseau_alexandre.libmusicPrettifier;
 
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.UnsupportedTagException;
-import java.io.File;
 import java.io.IOException;
 import org.json.JSONObject;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, UnsupportedTagException, InvalidDataException {
-        String directory = "/home/arousseau/Musique/Inconnu/Inconnu/";
-        File dir = new File(directory);
-        File[] files = dir.listFiles((dir1, name) -> name.endsWith(".mp3"));
-
-        for (File file : files) {
-            MyMp3File mp3file = new MyMp3File(file.getAbsolutePath());
-
-            for (MissingTag missing : mp3file.getMissingTags()) {
-                System.err.println(String.format("THis tag is missing: %s", missing));
-            }
-        }
+        Librairy librairy = new Librairy("/home/arousseau/Musique/Inconnu/Inconnu/");
         Discog api = new Discog();
         JSONObject json = api.search("Red Pill");
 
