b36136ab7c4993ab4228cba80cb468799e2c7bc7
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-03-29 15:02:33 +0200

    Use a temporary file to manipulate it

--- src/test/java/com/rousseau_alexandre/libmusicPrettifierTest/MyMp3FileTest.java
+++ src/test/java/com/rousseau_alexandre/libmusicPrettifierTest/MyMp3FileTest.java
@@ -4,14 +4,32 @@ import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.UnsupportedTagException;
 import com.rousseau_alexandre.libmusicPrettifier.MissingTag;
 import com.rousseau_alexandre.libmusicPrettifier.MyMp3File;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Vector;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyMp3FileTest {
 
+    private static final String MP3_SOURCE_FILEPATH = "src/test/resources/empty.mp3";
     private static final String MP3_FILEPATH = "src/test/resources/01 - Chris Orrick - Ten Year Party.mp3";
+
+    @Before
+    public void copyFile() throws IOException {
+        File from = new File(MP3_SOURCE_FILEPATH);
+        File to = new File(MP3_FILEPATH);
+        Files.copy(from.toPath(), to.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @After
+    public void removeFile() throws IOException {
+        Files.delete(new File(MP3_FILEPATH).toPath());
+    }
 
     @Test
     public void testMyMp3File() {
@@ -33,7 +51,13 @@ public class MyMp3FileTest {
     }
 
     @Test
-    public void testgetSearchStringFromFile() throws IOException, UnsupportedTagException, InvalidDataException {
+    public void testGetSearchStringFromFile() throws IOException, UnsupportedTagException, InvalidDataException {
+        MyMp3File file = new MyMp3File(MP3_FILEPATH);
+        Assert.assertEquals("Chris Orrick Ten Year Party", file.getSearchStringFromFile());
+    }
+
+    @Test
+    public void testCorrectFile() throws IOException, UnsupportedTagException, InvalidDataException {
         MyMp3File file = new MyMp3File(MP3_FILEPATH);
         Assert.assertEquals("Chris Orrick Ten Year Party", file.getSearchStringFromFile());
     }
